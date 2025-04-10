07caf4dbdc1ba1025661c80d9907b074890ed378
viprko <pr.and.vi@gmail.com>
2025-04-10 20:34:47 +0300

    Api gateway first stable version

---
+++ api-gateway/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "pet.gardenmanager"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.1")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "garden-manager"
-
+include("api-gateway")
